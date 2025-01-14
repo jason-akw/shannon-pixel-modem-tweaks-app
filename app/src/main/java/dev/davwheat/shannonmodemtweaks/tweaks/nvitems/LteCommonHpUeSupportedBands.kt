e8869d3832112166ada155180e2e339e8534e6ab
Jason Huang <akw2312@gmail.com>
2025-01-14 08:34:22 +0800

    Typo fix

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/LteCommonHpUeSupportedBands.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/LteCommonHpUeSupportedBands.kt
@@ -6,7 +6,7 @@ import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-class LteCommomHpUeSupportedBands : NvItemTweak(), Parcelable {
+class LteCommomnHpUeSupportedBands : NvItemTweak(), Parcelable {
   @IgnoredOnParcel
   override val name = "Enable HPUE PC2 on LTE"
 
